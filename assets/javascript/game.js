15386cf4ff452908bf5326cbf1c3caa4f06c7b5f
kiselblat <tsechrist@outlook.com>
2019-04-24 15:15:12 -0500

    game working beginning styling

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,5 +1,55 @@
-// hangman style guess the 
+// hangman style guess the word game
 
+// word list of arbitrary length
+var WORDS = ["processor", "memory", "pixel", "gigabyte", "motherboard", "qwerty", "binary", "keyboard", "mouse", "disk", "download", "digital", "electronic", "network", "server", "laptop", "input", "output", "circuit", "bandwidth"];
+
+// how many bad guesses
+var GUESSES = 10
+
+// global scoreboard
+var WINS = 0;
+var LOSSES = 0;
+
+// initialize variables
+var remainingTurns = GUESSES;
+var guessedLetters = [];
+var solution = [];
+var remainingLetters = 0;
+var word = "";
+var guessWord = "";
+var outcome = "ongoing";
+
+// refreshes the gameboard
+var updateGameboard = function() {
+  document.getElementById("guess-word").textContent = guessWord;
+  document.getElementById("guesses-left").textContent = remainingTurns;
+  document.getElementById("guessed-letters").textContent = guessedLetters;
+  document.getElementById("wins").textContent = WINS;
+  document.getElementById("losses").textContent = LOSSES;
+}
+
+// starts a new game
+var newGame = function() {
+  document.getElementById("results-banner").textContent = "";
+  outcome = "ongoing";
+  guessedLetters = [];
+  solution = [];
+  remainingLetters = 0;
+  remainingTurns = GUESSES;
+  word = WORDS[Math.floor(Math.random() * WORDS.length)];
+  console.log("picked ", word);
+  for (var i = 0; i < word.length; i++) {
+    solution[i] = "_";
+    remainingLetters ++;
+    }
+  guessWord = solution.join(" ");
+  console.log(solution);
+  console.log(remainingLetters);
+  console.log(remainingTurns);
+  updateGameboard();
+}
+
+// uses a regex to test that str is a lowercase alphanumeric letter
 var isValidLetter = function (str) {
   if (str.length === 1 && str.match(/[a-z]/i)) {
     return true;
@@ -8,66 +58,73 @@ var isValidLetter = function (str) {
   }
 }
 
-var WORDS = ["processor"];
-var guessedLetters = [];
-var guesses = 30;
-var solution = [];
-var remainingLetters = 0;
-var wins = 0;
-var losses = 0;
+// start the first game
+newGame();
 
-// 1. Pick a word
-var word = WORDS[Math.floor(Math.random() * WORDS.length)];
-console.log("picked ", word);
-
-for (var i = 0; i < word.length; i++) {
-  solution[i] = "_";
-  remainingLetters ++;
-}
-console.log(solution);
-console.log(remainingLetters);
-
-guessWord = solution.join(" ");
-document.getElementById("guess-word").textContent = guessWord;
-
+// wait for and store the user's guess
 document.onkeyup = function(event) {
-  // Determines which key was pressed.
   var guess = event.key;
   
-  guess = guess.toLowerCase();
-  console.log(guess)
-
-  if (!(isValidLetter(guess))) {
-    console.log("user didn't guess a letter");
-    return;
-  } else if (guessedLetters.includes(guess)) {
-    console.log("already guessed");
-    return;
-  } else {
-    console.log("user guessed: ", guess)
-  }
-
-  if (remainingLetters === 0) {
-    // YOU WIN
-   } else if (guesses === 0) {
-    // YOU LOSE
-   } else if (word.includes(guess)){
+  // if game isn't over
+  if (outcome === "ongoing") {
+    
+    // validate the user guess
+    guess = guess.toLowerCase();
+    if (!(isValidLetter(guess))) {
+      console.log("user didn't guess a letter");
+      return;
+    } else if (guessedLetters.includes(guess)) {
+      console.log("already guessed");
+      return;
+    } else {
+      console.log("user guessed: ", guess)
+    }
+    
+    // if the guess is in the word, reveal the letters and decrement the letter count
+    if (word.includes(guess)){
       for (var j = 0; j < word.length; j++) {
         if (word[j] === guess) {
-            solution[j] = guess;
-            guessWord = solution.join(" ");
-            document.getElementById("guess-word").textContent = guessWord;
-          } 
+          solution[j] = guess;
+          guessWord = solution.join(" ");
+          remainingLetters--;
+        }
       }
-   } else if (!(word.includes(guess))) {
-      guessedLetters.push(guess);
-      guesses--;
+    // otherwise store the wrong guess and take away a chance
+    } else if (!(word.includes(guess))) {
+    guessedLetters.push(guess);
+    remainingTurns--;
+    }
+    
+    console.log(solution);
+    console.log(remainingLetters);
+    console.log(remainingTurns);
+
+    // user wins if remaining letters reaches zero
+    if (remainingLetters === 0) {
+      WINS++;
+      updateGameboard();
+      document.getElementById("results-banner").textContent = "Congratulations! You scored a victory on behalf of humanity! Press space for a new game."
+      outcome = "win";
+      console.log(outcome);
+    // user loses when they run out of guesses
+    } else if (remainingTurns === 0) {
+      LOSSES++;
+      updateGameboard()
+      document.getElementById("results-banner").textContent = "Score one for the machines, you've lost! You were trying to guess " + word + ". Press space for a new game."
+      outcome = "loss";
+      console.log(outcome);
     }
 
-  document.getElementById("guesses-left").textContent = guesses;
-  document.getElementById("guessed-letters").textContent = guessedLetters;
-  document.getElementById("wins").textContent = wins;
-  document.getElementById("losses").textContent = losses;
-  
-      console.log(solution);
+  // if game is over wait for user to press space
+  } else if (!(outcome === "ongoing") && (guess === " ")) {
+    console.log("game over");
+    newGame();
+  } else if (!(outcome === "ongoing")) {
+    console.log("user didn't press space");
+    return;
   }
+
+  // refresh the display after doing all other game tasks
+  updateGameboard();
+}
+
